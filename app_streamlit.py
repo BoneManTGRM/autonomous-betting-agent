d7d86b58c6faf5edf994b76e961b57e92c21a50c
Reparodynamics <papadenico@yahoo.com>
2026-06-16 15:48:04 -0600

    Avoid duplicate sidebar brand in legacy entrypoint

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,18 +3,12 @@ from __future__ import annotations
 import streamlit as st
 
 APP_NAME = "ABA Signal Pro"
-APP_TAGLINE = "Powered by Reparodynamics"
 
 st.set_page_config(
     page_title=APP_NAME,
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
-with st.sidebar:
-    st.markdown("### :green[ABA] Signal :red[Pro]")
-    st.caption(APP_TAGLINE)
-    st.markdown("---")
 
 
 def _ignore_late_page_config(*args, **kwargs):
