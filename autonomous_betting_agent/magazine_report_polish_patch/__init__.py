baa2205816f558679724ec0232d6327f562c5e90
Reparodynamics <papadenico@yahoo.com>
2026-07-02 08:54:42 -0600

    Add package marker

---
+++ autonomous_betting_agent/magazine_report_polish_patch/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+_PATCH_VERSION = 'package_marker'
+
+
+def install() -> None:
+    return None
+
+
+def install_sale_ready_polish() -> None:
+    return None
+
+
+def install_live_odds_api_match() -> None:
+    return None
